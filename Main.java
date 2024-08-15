e655097a2e6708af4e1aac2942143f18d5dd52b4
KASHISH CHHABADIYA <chhabadiyakashish@gmail.com>
2024-08-15 23:24:59 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        greetUser("Alice");
+    }
+
+    public static void greetUser(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 }
