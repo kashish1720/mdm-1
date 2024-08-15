e4c0f556de349107d32d684d55d53f6ed02b9129
KASHISH CHHABADIYA <chhabadiyakashish@gmail.com>
2024-08-16 02:04:34 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
-package Kashish;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        newMethod(); // Adding a call to the new method
+    }
+    public static void newMethod() {
+        System.out.println("New method added!");
     }
 }
-
-    
-
